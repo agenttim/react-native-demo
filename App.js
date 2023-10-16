ccffcd2dd64c28bf365393247ae42501010521b7
agent_tim <agent_tim@mail.ru>
2023-10-16 08:29:00 +0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={ {color: '#3b5ec3'} }>Hello world! My name is Dexter!!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#abeaba',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
