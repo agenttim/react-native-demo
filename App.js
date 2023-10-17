80f2bf809d0bdf7d8da2ef9645102c0594daddfe
agent_tim <agent_tim@mail.ru>
2023-10-17 09:48:38 +0500

    добавили функцию скролла

--- App.js
+++ App.js
@@ -1,25 +1,29 @@
 import React, {useState} from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
 import {Navbar} from "./src/Navbar";
 import {AddTodo} from "./src/AddTodo";
 import {Todo} from "./src/Todo";
 
 export default function App() {
-    const [todos, setTodos] = useState([])
+    const [todos, setTodos] = useState([
+        {id: 1, title: 'Задача 1'},
+        {id: 2, title: 'Задача 2'},
+        {id: 3, title: 'Задача 3'},
+        {id: 4, title: 'Задача 4'},
+        {id: 5, title: 'Задача 5'},
+        {id: 6, title: 'Задача 6'},
+        {id: 7, title: 'Задача 7'},
+        {id: 8, title: 'Задача 8'},
+        {id: 9, title: 'Задача 9'},
+        {id: 10, title: 'Задача 10'},
+        {id: 11, title: 'Задача 11'},
+        {id: 12, title: 'Задача 12'},
+        {id: 13, title: 'Задача 13'},
+        {id: 14, title: 'Задача 14'},
+        {id: 15, title: 'Задача 15'},
+    ])
 
     const addTodo = (title) => {
-
-/*        const newTodo = {
-            id: Date.now().toString(),
-            title: title
-        }
-
-        setTodos((prevTodos) => {
-            return [
-                ...prevTodos,
-                newTodo
-            ]
-        })*/
 
         setTodos(prev => [
             ...prev, {
@@ -34,11 +38,11 @@ export default function App() {
             <View style={styles.container}>
                 <AddTodo onSubmit={addTodo} />
 
-                <View>
-                    {todos.map(todo => (
-                        <Todo todo={todo} key={todo.id} />
-                    ))}
-                </View>
+                <FlatList
+                    keyExtractor={item => item.id.toString()}
+                    data={todos}
+                    renderItem={({item}) => (<Todo todo={item} />)}
+                />
             </View>
         </View>
     );
