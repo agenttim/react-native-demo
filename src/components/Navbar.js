7548e1a1e08701c21bea163b56fd35bb4b7c9be5
agent_tim <agent_tim@mail.ru>
2023-10-19 14:29:44 +0500

    Оптимизировал navbar для разных платформ

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React from "react";
-import {View, StyleSheet} from "react-native";
+import {View, StyleSheet, Platform} from "react-native";
 import {THEME} from "../theme";
 import {AppTextBold} from "./ui/AppTextBold";
 
 export const Navbar = ({title}) => {
     return (
-        <View style={styles.navbar}>
+        <View style={{...styles.navbar, ...Platform.select({
+                ios: styles.navbarIOs,
+                android: styles.navbarIOs
+            })}}>
             <AppTextBold style={styles.text}>{title}</AppTextBold>
         </View>
 
@@ -17,12 +20,17 @@ const styles = StyleSheet.create({
         height: 70,
         alignItems: 'center',
         justifyContent: 'flex-end',
-        backgroundColor: THEME.MAIN_COLOR,
         paddingBottom: 10
     },
+    navbarIOs: {
+        borderBottomColor: THEME.MAIN_COLOR,
+        borderBottomWidth: 1
+    },
+    navbarAndroid: {
+        backgroundColor: THEME.MAIN_COLOR,
+    },
     text: {
-        color: 'white',
+        color: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff',
         fontSize: 20
-
     }
 })
